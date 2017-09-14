2b760cfd5a5a63c5b1bf2888a80fbfb1d4c1d600
Michel Dumontier <michel.dumontier@gmail.com>
2017-09-14 16:05:52 +0900

    added labels to output, where available

--- es/1_prepare_data.php
+++ es/1_prepare_data.php
@@ -23,6 +23,16 @@ while($l = fgets($fp))
 }
 fclose($fp);
 
+if($in == "uniprot") {
+	$file = "proteins.csv";
+	$fp = fopen($file,"r");
+	fgetcsv($fp); // header
+	while($a = fgetcsv($fp)) {
+		$labels[ $a[0] ] = $a[1];
+	}
+}
+
+
 $count = count($ids);
 echo $count.PHP_EOL;
 
@@ -32,21 +42,23 @@ while($l = fgets($fp,20000))
 {
 	$a = explode(" ",$l);
 	$id = $a[0];
+	$label = '';
 	$uri = substr($ids[$id],1,-1);
 	if(strstr($uri,"http://purl.uniprot.org/uniprot/")) {
 		$id = "uniprot.".substr($uri,strlen("http://purl.uniprot.org/uniprot/"));
 		if(strstr($uri,"#")) {
 			continue;
 		}
+		if(isset($labels[$uri])) $label = '"label":"'.$labels[ $uri ].'",';
 	}
-	
+
 	array_shift($a);
 	$buf = '';
 	foreach($a as $k => $v) {$buf .= "$k|$v ";}
 	$buf = trim($buf);
 	
-	$e =  '{"index":{"_index":"vector", "_type":"'.$in.'", "_id": "'.$id.'"}}'.PHP_EOL;
-	$e .= '{"name": "'.$uri.'", "@model_factor":"'.$buf.'"}'.PHP_EOL;
+	$e =  '{"index":{"_index":"vector","_type":"'.$in.'","_id":"'.$id.'"}}'.PHP_EOL;
+	$e .= '{'.$label.'"name": "'.$uri.'","@model_factor":"'.$buf.'"}'.PHP_EOL;
 	fwrite($fout,$e);
 }
 fclose($fp);
